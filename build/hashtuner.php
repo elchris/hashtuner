6881de4e8b7b86b24e2f4ca5c5d2305da95c3016
elchris <frenchy@gmail.com>
2019-12-22 17:26:10 -0600

    added support for "box" with box.json , i can now just use box with force-autodiscovery to generate the phar file with the necessary vendor dependencies
    https://github.com/humbug/box

--- build/hashtuner.php
+++
@@ -1,2 +0,0 @@
-<?php
-//work in progress.
--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
+require_once 'vendor/autoload.php';
 
 use ChrisHolland\HashTuner\Tuner;
 
-require_once './vendor/autoload.php';
 echo "Settings for Argon2id\n";
 print Tuner::getTunedArgonSettings()->toJson()."\n";
